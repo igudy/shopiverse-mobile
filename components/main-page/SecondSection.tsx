4f63e1437b5d922870cecb3e9147d0a52f371856
goodness-dev <goodness@bepeerless.co>
2024-10-28 01:32:40 +0100

    feat: dummy different section added

---
+++ components/main-page/SecondSection.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SecondSection = () => {
+  return (
+    <View>
+      <Text>SecondSection</Text>
+    </View>
+  )
+}
+
+export default SecondSection
---
+++ components/main-page/ThirdSection.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ThirdSection = () => {
+  return (
+    <View>
+      <Text>ThirdSection</Text>
+    </View>
+  )
+}
+
+export default ThirdSection
